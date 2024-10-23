629d8ee70769f6ff3a2663ca7305395598fde726
audran-vibly <audranvibly@gmail.com>
2024-10-23 13:54:43 +0200

    remove nullable

--- database/migrations/2024_01_19_121237_create_categories_table.php
+++ database/migrations/2024_01_19_121237_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->string('slug')->unique()->nullable();
             $table->char('color', 7)->default('#ffffff');
             $table->timestamps();
